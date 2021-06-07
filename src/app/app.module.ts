ded43ac7cd806ca2816059e22cdb8ae6d0fa97f5
Asmaa <asmaaayman159753@gmail.com>
2021-06-08 01:48:41 +0200

    starting Asmaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/User/login/login.component';
+import { ForgotPasswordComponent } from './components/User/forgot-password/forgot-password.component';
+import { RegistartionComponent } from './components/User/registartion/registartion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    RegistartionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
