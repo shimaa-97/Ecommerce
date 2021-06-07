13aec7e3b5b23e9e8fc9a21a7db3a67ca0990c19
shimaa ismail <shimaa.ismail.011@gmail.com>
2021-06-07 14:18:29 +0200

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GoogleMapsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
